341f059831fef6af66b18c29c217ef7a30a957c2
ppuech_ssg <paul.puech@soprasteria.com>
2025-08-17 12:27:46 +0200

    ✨ Ajout get tickets v1

--- app/lists.py
+++ app/lists.py
@@ -2,6 +2,8 @@ import typer
 from rich.console import Console
 from rich.table import Table
 
+from .config import JIRA_PROJECT
+
 app = typer.Typer(help="Manage projects")
 console = Console()
 
@@ -17,6 +19,27 @@ def project(ctx: typer.Context) -> None:
 
         console.print(table)
 
+    except (ConnectionError, TimeoutError, PermissionError) as e:
+        typer.echo(f"Erreur : {e}", err=True)
+        raise typer.Exit(code=1) from e
+
+
+@app.command()
+def ticket(ctx: typer.Context) -> None:
+    try:
+        jira = ctx.obj
+
+        jql = f"project = {JIRA_PROJECT} AND status NOT IN (Closed, Resolved) ORDER BY created DESC"
+
+        issues = jira.search_issues(jql_str=jql, startAt=0, maxResults=10, fields="key,summary,assignee,status,created")
+
+        table = Table("Code", "Nom", "Statut", "Responsable")
+        for issue in issues:
+            table.add_row(
+                issue.key, issue.fields.summary, issue.fields.status.name, getattr(issue.fields.assignee, "displayName", "None")
+            )
+
+        console.print(table)
     except (ConnectionError, TimeoutError, PermissionError) as e:
         typer.echo(f"Erreur : {e}", err=True)
         raise typer.Exit(code=1) from e
